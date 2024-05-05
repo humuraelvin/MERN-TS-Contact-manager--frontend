5fad29698acc58d6eb3d48e5913f912c9b0c4c12
HUMURA ELVIN <148461063+humuraelvin@users.noreply.github.com>
2024-05-05 12:13:09 +0200

    Update main.tsx

--- src/main.tsx
+++ src/main.tsx
@@ -10,6 +10,7 @@ import EditContact from "./components/EditContact.tsx";
 import { BrowserRouter, Routes, Route } from "react-router-dom";
 
 
+
 ReactDOM.createRoot(document.getElementById("root")!).render(
   <React.StrictMode>
     <App />
